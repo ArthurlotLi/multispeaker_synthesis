19421f8e9af50ee6aa6598d358f32ff3aae05400
Arthur <Artieli007@gmail.com>
2022-04-15 16:06:20 -0700

    Increase target dBFS.

--- vocoder/sv2tts_inference.py
+++ vocoder/sv2tts_inference.py
@@ -27,7 +27,7 @@ class SV2TTSBridge:
   # How many samples of 0s of silence for pauses between mels. 
   _silence_spaces = 10
   _add_silences = False
-  _norm_target_dBFS = -12 # Everything on TV is between -12 and -20.
+  _norm_target_dBFS = -5 # Everything on TV is between -12 and -20.
  
   def __init__(self, load_immediately=True):
     self._weights_fpath = str(Path(__file__).parent.resolve().joinpath(self._weights_fpath))
